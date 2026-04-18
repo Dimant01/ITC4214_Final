b2f5840cf71d6c3fc576f12117a0cae9a7763967
dmantzaridis <dmantzaridis@gmail.com>
2026-04-18 19:19:18 +0300

    Admin crashing due to lack of book image fix

--- Bookyrinth/catalog/admin.py
+++ Bookyrinth/catalog/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import Book, Category, Tag
 
-# Register your models here.
+#Register your models here
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "price", "stock", "safe_image")
+
+    def safe_image(self, obj):
+        if obj.image:
+            try:
+                return obj.image.url
+            except:
+                return "Broken image"
+        return "No image"
+
+    safe_image.short_description = "Image"
+
+
 admin.site.register(Category)
 admin.site.register(Tag)
