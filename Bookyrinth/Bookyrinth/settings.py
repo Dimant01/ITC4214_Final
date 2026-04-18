1d89f7e33d0d44bb2020a230974888b7b125f80b
dmantzaridis <dmantzaridis@gmail.com>
2026-04-18 18:39:16 +0300

    Made a change for better Cloudinary authentication

--- Bookyrinth/Bookyrinth/settings.py
+++ Bookyrinth/Bookyrinth/settings.py
@@ -147,7 +147,7 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 cloudinary.config(
-    cloud_name="CLOUDINARY_CLOUD_NAME",
-    api_key="CLOUDINARY_API_KEY",
-    api_secret="CLOUDINARY_API_SECRET",
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
 )
